7c90ddff9ffb26d673e32b0f80e6a7d1c13be200
Jason Ribble <jason@companiontechnology.net>
2022-11-14 16:58:45 -0700

    fix: start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-sudo mkdir -p /docker && cd /docker
-sudo git clone https://github.com/jasonribble/relay
 sudo mkdir -p /docker/relay/node-config
 cd /docker/relay/node-config
 sudo wget https://book.world.dev.cardano.org/environments/mainnet/alonzo-genesis.json
