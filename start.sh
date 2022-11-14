9f469a8329c2ad3d3969284d3d66b2efb9fe3959
Jason Ribble <jason@companiontechnology.net>
2022-11-14 14:03:27 -0700

    fix: forgot sudo on one

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ sudo git clone https://github.com/jasonribble/relay
 sudo mkdir -p /docker/relay/node-config
 cd /docker/relay/node-config
 sudo wget https://book.world.dev.cardano.org/environments/mainnet/alonzo-genesis.json
-wget https://book.world.dev.cardano.org/environments/mainnet/byron-genesis.json
+sudo wget https://book.world.dev.cardano.org/environments/mainnet/byron-genesis.json
 sudo wget https://book.world.dev.cardano.org/environments/mainnet/shelley-genesis.json
 sudo wget https://book.world.dev.cardano.org/environments/mainnet/config.json
 sudo wget https://book.world.dev.cardano.org/environments/mainnet/topology.json
