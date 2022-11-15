0192bc608d11e8436edbe28d25e4b1182d572dd6
Jason Ribble <jason@companiontechnology.net>
2022-11-14 17:38:16 -0700

    fix: typo in producer docker file

--- node-config/cardano-relay.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/cardano-node run +RTS -N -A16m -qg -qb -RTS --topology /config/topology.json --database-path /data/db --socket-path /ipc/node.socket --host-addr 0.0.0.0 --port 3001 --config /config/config.json
